bf34a9ab1ed060672fae898c90eeeef092ed193d
Akash Vaghela <46647604+akashvaghela09@users.noreply.github.com>
2024-05-19 19:21:15 +0530

    ADD 1.0 tic tac token

--- src/Fizzbuzz.sol
+++ src/Fizzbuzz.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
+
 contract Fizzbuzz {
     function fizzbuzz(uint256 n) public pure returns (string memory) {
-        if (n == 5) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "fizzbuzz";
+        }
+
+        if (n % 5 == 0) {
             return "buzz";
         }
 
-        if (n == 3) {
+        if (n % 3 == 0) {
             return "fizz";
         }
 
-        return string(n);
+        return Strings.toString(n);
     }
 }
--- test/Fizzbuzz.t.sol
+++ test/Fizzbuzz.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Fizzbuzz} from "../src/Fizzbuzz.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract Fizzbuzztest is Test {
     Fizzbuzz internal fizzbuzz;
@@ -17,15 +18,29 @@ contract Fizzbuzztest is Test {
         assertNotEq(two, 3);
     }
 
-    function test_returns_fizz_when_divisible_by_three() public view {
-        assertEq(fizzbuzz.fizzbuzz(3), "fizz");
+    function test_returns_fizz_when_divisible_by_three(uint256 n) public view {
+        vm.assume(n % 3 == 0);
+        vm.assume(n % 5 != 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "fizz");
     }
 
-    function test_returns_buzz_when_divisible_by_five() public view {
-        assertEq(fizzbuzz.fizzbuzz(5), "buzz");
+    function test_returns_buzz_when_divisible_by_five(uint256 n) public view {
+        vm.assume(n % 3 != 0);
+        vm.assume(n % 5 == 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "buzz");
     }
 
-    function test_returns_number_as_string_otherwise() public view {
-        assertEq(fizzbuzz.fizzbuzz(7), "7");
+    function test_returns_fizzbuzz_when_divisible_by_three_and_five(
+        uint256 n
+    ) public view {
+        vm.assume(n % 3 == 0);
+        vm.assume(n % 5 == 0);
+        assertEq(fizzbuzz.fizzbuzz(n), "fizzbuzz");
+    }
+
+    function test_returns_number_as_string_otherwise(uint256 n) public view {
+        vm.assume(n % 3 != 0);
+        vm.assume(n % 5 != 0);
+        assertEq(fizzbuzz.fizzbuzz(n), Strings.toString(n));
     }
 }
